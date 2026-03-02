23ed252d8ed6494cdb9f2f31fc4be9c708a1c14a
Bueno <teteubuenor@gmail.com>
2026-03-02 11:11:21 +0000

    adicionado os console.log

---
+++ variaveis/pessoa.js
@@ -0,0 +1,19 @@
+//var,const, let
+
+let nome  = "Matheus"
+let sobrenome = "Bueno"
+const cpf = "238.925.790-19"
+const rg = "1237134"
+let idade = "15"
+let estadocivil = "solteiro "
+let nota = 8 + 2
+let cansaço = true
+
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(estadocivil)
+console.log(nota)
+console.log(cansaço)
